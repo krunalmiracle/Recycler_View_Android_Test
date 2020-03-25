00aa5e68d72a8d3521548d83723e623e24750dfb
krunal badsiwal <krunalmiracle@gmail.com>
2020-03-25 20:35:16 +0100

    Initial Commit Initial commit with correct git ignore added to .gitignore in both the build and the gradle

---
+++ app/src/main/java/edu/upc/recycler_view_test/MainActivity.java
@@ -0,0 +1,14 @@
+package edu.upc.recycler_view_test;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
